5254fbfc33038487733c53bf18b3151218a7d206
mscasallasm <stiven_casallas@hotmail.com>
2023-01-20 10:09:31 -0500

    Clase 16 Qué son los logs y cómo usarlos: Se aprende sobre que son los logs y cuales son los diferentes niveles que se suelen usar en spring, se aprendio a cambiar estas propiedades en el archivo porperties que nos provee spring, se puede cambiar entre INFO, DEBUG, ERROR, a partir de ahi se muestra en el servidor de acuerdo a lo que se asigne

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -1,6 +1,11 @@
 package com.fundamentosplatzi.springboot.fundamentos.bean;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
+
+    Log LOGGER = LogFactory.getLog(MyBeanWithDependencyImplement.class);
 
     private MyOperation myOperation;
 
@@ -10,7 +15,9 @@ public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
 
     @Override
     public void printWithDependency() {
+        LOGGER.info("Hemos ingresado al metodo printWithDependency");
         int numero = 1;
+        LOGGER.debug("El numero enviado como parametro a la dependencia operation es : " + numero);
         System.out.println(myOperation.sum(numero));
         System.out.println("Hola desde la implementación de un bean con dependencia");
     }
