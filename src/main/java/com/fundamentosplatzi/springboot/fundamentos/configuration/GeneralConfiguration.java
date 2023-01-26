79476330752dfd717de6e38b5e65f5bbe18bfa01
mscasallasm <stiven_casallas@hotmail.com>
2023-01-26 11:37:07 -0500

    Clase 18 Configuración de datasource con properties y clases: Se aprendio a crear la configuracion de datasource a nivel de properties y a nivel de clases, a nivel de properties se realizo en application.properties que nos provee el inicializador de spring boot y a nivel de clases se realizo en una configuración general que se tenia previamente, se aprendio a configurar el nombre de usuario, contraseña, driver y url de base de datos.

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -5,6 +5,7 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithPropertiesImp
 import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,5 +25,16 @@ public class GeneralConfiguration {
     @Bean
     public MyBeanWithProperties function(){
         return new MyBeanWithPropertiesImplement(name, apellido);
+    }
+
+
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
     }
 }
