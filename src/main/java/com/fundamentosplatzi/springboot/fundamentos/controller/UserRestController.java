ce7a3759aef0ca7677cecac23d9506918a58fe87
mscasallasm <stiven_casallas@hotmail.com>
2023-06-01 14:02:27 -0500

    Clase 30 Pagination con Spring Boot: Se aprendio acerca de la paginación y como crear el servicio para paginar información a partir de ciertos parametros, sirve mucho para asociación con el front el listados o cosas similares.

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/controller/UserRestController.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/controller/UserRestController.java
@@ -5,6 +5,8 @@ import com.fundamentosplatzi.springboot.fundamentos.caseuse.DeleteUser;
 import com.fundamentosplatzi.springboot.fundamentos.caseuse.GetUser;
 import com.fundamentosplatzi.springboot.fundamentos.caseuse.UpdateUser;
 import com.fundamentosplatzi.springboot.fundamentos.entity.User;
+import com.fundamentosplatzi.springboot.fundamentos.repository.UserRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,12 +20,14 @@ public class UserRestController {
     private CreateUser createUser;
     private DeleteUser deleteUser;
     private UpdateUser updateUser;
+    private UserRepository userRepository;
 
-    public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser, UpdateUser updateUser) {
+    public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser, UpdateUser updateUser, UserRepository userRepository) {
         this.getUser = getUser;
         this.createUser = createUser;
         this.deleteUser = deleteUser;
         this.updateUser = updateUser;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/")
@@ -45,6 +49,11 @@ public class UserRestController {
     @PutMapping("/{id}")
     ResponseEntity<User> replaceUser(@RequestBody User newUser, @PathVariable Long id){
         return new ResponseEntity<>(updateUser.update(newUser, id), HttpStatus.OK);
+    }
+
+    @GetMapping("/pageable")
+    List<User> getUserPegeable(@RequestParam int page, @RequestParam int size) {
+        return userRepository.findAll(PageRequest.of(page, size)).getContent();
     }
 
 }
